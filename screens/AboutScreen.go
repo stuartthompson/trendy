945e53a3e0d20ef0457bb3db0aef005a5f1a6331
Stu Thompson <stuart@stuartthompson.net>
2018-05-14 09:35:12 -0700

    Adds boilerplate main and about screen.

---
+++ screens/AboutScreen.go
@@ -0,0 +1,37 @@
+// Copyright 2018 Stuart Thompson.
+
+// This file is part of Trendy.
+
+// Trendy is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// Trendy is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with Trendy.  If not, see <http://www.gnu.org/licenses/>.
+
+package screens
+
+import (
+	"github.com/stuartthompson/trendy/io"
+)
+
+// AboutScreen ...
+type AboutScreen struct {
+}
+
+// Render ...
+// Renders the about screen.
+func (s *AboutScreen) Render() {
+	io.ClearScreen(0)
+	width, height := io.GetWindowSize()
+	io.RenderPaneBorder(0, 0, width-1, height-1, 212, 0)
+	io.RenderText("About", 1, 1, 255, 0)
+	io.RenderText("Trendy is a news aggregator.", 1, 3, 255, 0)
+	io.Flush()
+}
